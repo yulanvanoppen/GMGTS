f71e8011f65e8c804fa9f76e7af058ece9530768
Yulan van Oppen <yulanvanoppen@gmail.com>
2024-10-28 17:34:34 +0100

    Updated smoothing

--- TEST_maturation.m
+++ TEST_maturation.m
@@ -12,8 +12,8 @@ close all
 load('system_maturation_delay.mat')
 
 first_obs = 1;
-dt = 10;
-noise_level = .05;
+dt = 20;
+noise_level = .1;
 seed = 1;
 
 generator = Generator(system ...                                            % generator setup
@@ -54,12 +54,13 @@ methods = [methods "GMGTS"];
 %                       );
 %                   
 estimator = Estimator(data, system ...                                      % estimator setup
-                      , 'Stages', 0 ...                                     % 0: smoothing only, 1: first stage only
+                      , 'Stages', 2 ...                                     % 0: smoothing only, 1: first stage only
                       , 'Methods', methods ...                              % GMGT, GTS, or both
                       , 'Knots', linspace(0, 200, 5) ...
                       , 'PenalizedInterval', [0 200] ...
                       , 'TimePoints', data.t ...
-                      , 'InteractiveSmoothing', true ...
+                      , 'Lambda', 1e-10 ...
+                      ...%, 'InteractiveSmoothing', true ...
                       );
 
 estimator.estimate();
